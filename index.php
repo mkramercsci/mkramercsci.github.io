1fa29af0d333843072918e270af3fd8776e7d16f
BuildTools <unconfigured@null.spigotmc.org>
2022-01-08 18:01:40 -0600

    adding css

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<<<<<<< HEAD
-<h1>hello world</h>
-=======
-<h1>test2</h>
->>>>>>> b6d9280585ddfd9401c8497de571aa159f091c2e
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
