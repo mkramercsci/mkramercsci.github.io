dafd830b61d603cd02f6342cb9453550fb4b6f64
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 17:50:15 -0600

    adjusted homepage icon size

---
+++ about.php
@@ -0,0 +1,4 @@
+<div class="main">
+ <h1>Portfolio</h1>
+ <p>This is the homepage.</p>
+</div>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <body>
   <!-- Side navigation -->
  <div class="sidenav">
+   <img src="images\car.png"></img>
    <a href="#">About</a>
    <a href="#">Services</a>
    <a href="#">Clients</a>
@@ -14,7 +15,6 @@
 
  <!-- Page content -->
  <div class="main">
-   <img src="images\car.png"></img>
   <h1>This is a new heading</h1>
   <p>This is a new paragraph.</p>
  </div>
