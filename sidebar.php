5dac722f81bc78452e651526d7a3f43cc3d4e034
BuildTools <unconfigured@null.spigotmc.org>
2022-01-16 14:18:33 -0600

    added storyboards page & link in sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@ echo
  <div class="sidenav">
    <a href="index.php"><img src="images\car.png"></img></a>
    <a href="about.php">About</a>
-   <a href="#">Storyboards</a>
+   <a href="storyboards.php">Storyboards</a>
    <a href="#">Character Design</a>
    <a href="#">Contact</a>
  </div>';
---
+++ storyboards.php
@@ -0,0 +1,16 @@
+<?php include 'sidebar.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+ <!-- Page content -->
+ <div class="main">
+  <h1>Storyboards</h1>
+  <p>The storyboard projects page.</p>
+ </div>
+
+</body>
+</html>
