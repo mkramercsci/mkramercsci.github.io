b26ac57d617a42de8d862ce0b37e24a416125a7b
Michaela <mkramer.csci@gmail.com>
2022-09-17 14:54:10 -0600

    added styling to about page

---
+++ character-design.php
@@ -0,0 +1,16 @@
+<?php include 'sidebar.php'; ?>
+<html>
+<head>
+	<link rel="stylesheet" href="styles.css">
+	<script src="script.js" defer></script>
+</head>
+
+<body>
+	
+<h1>Character Design</h1>
+<div class="main">
+
+</div>
+
+</body>
+</html>
--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@ echo
    <a href="index.php"><img src="portfolio\icon\car.png"></img></a>
    <a href="about.php">About</a>
    <a href="storyboards.php">Storyboards</a>
-   <a href="#">Character Design</a>
+   <a href="character-design.php">Character Design</a>
    <a href="#">Contact</a>
  </div>';
 ?>
